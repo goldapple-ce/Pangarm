39ac4bf8976b6f60defe6d25e889c2fbe94bd50d
김도현 <nanayagoon@daum.net>
2024-03-27 18:07:02 +0900

    .
    Former-commit-id: 403f01d173033e0ba048d47a5b5ac41044b09597
    Former-commit-id: 9c20521de6127041f4ddd28e92e6b55f800f3138

--- precedent_search/search/service.py
+++ precedent_search/search/service.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from kafka import KafkaProducer,KafkaConsumer
 import json
 import re
 from hdfs import InsecureClient
 import os
 from bs4 import BeautifulSoup
-
+import uuid
+import  threading
 HDFS_URL = os.environ["HDFS_URL"]
 HDFS_USER = os.environ["HDFS_USER"]
 client = InsecureClient(HDFS_URL, user=HDFS_USER)
@@ -29,43 +30,92 @@ embedding_model = HuggingFaceEmbeddings(
     model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs
 )
-vectorstore = Chroma(persist_directory="ml/model", embedding_function=embedding_model)
-retriever = vectorstore.as_retriever()
+producer = KafkaProducer(
+    acks=0, # 메시지 전송 완료에 대한 체크
+    compression_type='gzip', # 메시지 전달할 때 압축(None, gzip, snappy, lz4 등)
+    bootstrap_servers=['kafka2.wookoo.shop:9092',"kafka3.wookoo.shop:9092"], # 전달하고자 하는 카프카 브로커의 주소 리스트
+    value_serializer=lambda x:json.dumps(x).encode('utf-8') # 메시지의 값 직렬화
+)
 
+consumer = KafkaConsumer(
+    "response",
+    group_id='test2',  # Consumer 그룹 ID
+    bootstrap_servers=['kafka2.wookoo.shop:9092',"kafka3.wookoo.shop:9092"],
+    auto_offset_reset='earliest',  # 소비자 그룹이 처음으로 오프셋을 설정할 때 어떻게 처리할지 설정
+)
+
+kafka_response = {}
+
+def kafkaConsume():
+    print("start Thread")
+    while True:
+        for message in consumer:
+            key = message.key.decode('utf-8') if message.key else None
+            value = message.value.decode('utf-8') if message.value else None
+            response = json.loads(value)
+            print(value,response)
+            kafka_response[response["id"]] = response
+
+thread = threading.Thread(target=kafkaConsume)
+thread.start()
 
 def search(search_request):
     # 여기서 서비스레이어가 돌아간다.
     content = search_request['content'].value
     count = search_request['count'].value
 
-    fileList = [(re.sub(r'[^0-9]', '', i[0].metadata['source']) + ".html") for i in
-                vectorstore.similarity_search_with_score(content, k=count)]
-    #return Response({"data":fileList}, status=status.HTTP_200_OK)
-
-    # print(j[0].metadata)
-    fileList = [(re.sub(r'[^0-9]', '', i[0].metadata['source']) + ".json", i[1]) for i in
-                vectorstore.similarity_search_with_score(content, k=count)]
-    data = []
-    keys = ["info", "summary", "judgement", "conclusion", "keywords"]
-
-    for fileName, score in fileList:
-        json_data = {"score": score}
-        with client.read(f"/data/json/{fileName}", encoding="utf8") as f:
-            t = json.load(f)
-
-            for key in keys:
-                try:
-                    json_data[key] = t[key]
-                except:
-                    json_data[key] = None
-
-            if (json_data["keywords"] == None):
-                json_data["keywords"] = []
-
-            data.append(json_data["info"]["caseNumber"])
-    response = {
-        "data": data
+    vector = embedding_model.embed_query(content)
+    key = str(uuid.uuid4())
+    kafkaRequest = {
+        "id":key,
+        "request" : "search",
+        "vector" : vector,
+        "count" : count
     }
+
+
+    print("hahaha")
+    producer.send("request",value=kafkaRequest)
+    while not key in kafka_response:
+       pass
+
+    response = {"data":kafka_response[key]['data']}
+    del kafka_response[key]
+
+
+
+
+
+
+
+    # fileList = [(re.sub(r'[^0-9]', '', i[0].metadata['source']) + ".html") for i in
+    #             vectorstore.similarity_search_with_score(content, k=count)]
+    # #return Response({"data":fileList}, status=status.HTTP_200_OK)
+    #
+    # # print(j[0].metadata)
+    # fileList = [(re.sub(r'[^0-9]', '', i[0].metadata['source']) + ".json", i[1]) for i in
+    #             vectorstore.similarity_search_with_score(content, k=count)]
+    # data = []
+    # keys = ["info", "summary", "judgement", "conclusion", "keywords"]
+    #
+    # for fileName, score in fileList:
+    #     json_data = {"score": score}
+    #     with client.read(f"/data/json/{fileName}", encoding="utf8") as f:
+    #         t = json.load(f)
+    #
+    #         for key in keys:
+    #             try:
+    #                 json_data[key] = t[key]
+    #             except:
+    #                 json_data[key] = None
+    #
+    #         if (json_data["keywords"] == None):
+    #             json_data["keywords"] = []
+    #
+    #         data.append(json_data)
+    # response = {
+    #     "data": data
+    # }
 
     return Response(response, status=status.HTTP_200_OK)
 
