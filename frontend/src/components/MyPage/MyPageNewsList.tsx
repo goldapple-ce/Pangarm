9e93d9f03b622050fe36f188adf840ef8f66d2c0
김민준 <alswns1285@gmail.com>
2024-04-03 15:55:45 +0900

    Ref: FE_마이페이지 뉴스 무한 스크롤 구현
    S10P22A509-274

--- frontend/src/components/MyPage/MyPageNewsList.tsx
+++ frontend/src/components/MyPage/MyPageNewsList.tsx
@@ -1,34 +1,53 @@
+import React, { useEffect } from "react";
+
+import { useInView } from "react-intersection-observer";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { News } from "@/types";
 import { getCategoryNewsList } from "@/services/newsService";
-import { useQuery } from "@tanstack/react-query";
-import MyPageNewsListItem from "./MyPageNewsListItem";
+
+import MyPageNewsListItem from "@/components/MyPage/MyPageNewsListItem";
+import LoadingAnimation from "@/components/LoadingAnimation";
+import Error500Animation from "@/components/Error/Error500Animation";
 
 type MyPageNewsListProps = {
   currentCategory: string;
 };
 
-interface CategoryNewsListItemProps {
-  id: string;
-  title: string;
-  content: string;
-  imageUrl: string;
-  createdAt: string;
-}
-
 export default function MyPageNewsList({
   currentCategory,
 }: MyPageNewsListProps) {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["categoryNewsList", currentCategory],
-    queryFn: () => getCategoryNewsList(currentCategory, 0, 10),
-  });
+  const { data, hasNextPage, isLoading, fetchNextPage, error } =
+    useInfiniteQuery({
+      queryKey: ["getNewsListOfCategory", currentCategory],
+      queryFn: ({ pageParam = 0 }) =>
+        getCategoryNewsList(currentCategory, pageParam, 12),
+      initialPageParam: 0,
+      getNextPageParam: (_lastPage, allPages) => {
+        return allPages.length + 1;
+      },
+    });
+
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
 
   if (isLoading) {
-    return <div></div>;
+    return (
+      <div className="w-full">
+        <LoadingAnimation />
+      </div>
+    );
   }
 
   if (error) {
-    return <div></div>;
+    return (<div className="w-9/12"><Error500Animation /></div>);
   }
+
   if (currentCategory === "") {
     return (
       <div className="flex h-full w-9/12 items-center justify-center font-TitleMedium text-2xl">
@@ -37,7 +56,7 @@ export default function MyPageNewsList({
     );
   }
 
-  if (data?.data.data.length === 0) {
+  if (data?.pages.every((el) => el.data.data.length == 0)) {
     return (
       <div className="flex h-full w-9/12 items-center justify-center font-TitleMedium text-2xl">
         {currentCategory} 카테고리의 기사가 없습니다...
@@ -47,24 +66,21 @@ export default function MyPageNewsList({
 
   return (
     <div className=" m-2 flex w-9/12 flex-wrap overflow-y-scroll">
-      {data?.data.data.map(
-        ({
-          id,
-          title,
-          content,
-          imageUrl,
-          createdAt,
-        }: CategoryNewsListItemProps) => (
-          <MyPageNewsListItem
-            key={id}
-            id={id}
-            title={title}
-            content={content}
-            imageUrl={imageUrl}
-            createdAt={createdAt}
-          />
-        ),
-      )}
+      {data?.pages.map((group, i) => (
+        <React.Fragment key={i}>
+          {group.data.data.map((newsItem: News) => (
+            <MyPageNewsListItem
+              key={newsItem.id}
+              id={newsItem.id}
+              title={newsItem.title}
+              content={newsItem.content}
+              imageUrl={newsItem.imageUrl}
+              createdAt={newsItem.createdAt}
+            />
+          ))}
+        </React.Fragment>
+      ))}
+      <div ref={ref} className="h-2 w-full" />
     </div>
   );
 }
